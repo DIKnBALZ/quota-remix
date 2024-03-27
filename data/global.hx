104e44242e3f5dfde44edc4cee18ebc1a6202e6c
cookie ;P <85509162+cookiesock@users.noreply.github.com>
2024-03-27 13:43:57 -0500

    accidentally disabled window resizing

--- data/global.hx
+++ data/global.hx
@@ -4,7 +4,6 @@ import hxvlc.util.Handle;
 function new() {
     FlxG.width = FlxG.initialWidth = 960;
 	window.resize(FlxG.width, FlxG.height);
-	window.resizable = false;
 	for (camera in FlxG.cameras.list) camera.setSize(FlxG.width, FlxG.height);
 
 	window.x = (Capabilities.screenResolutionX / 2) - (window.width / 2);
@@ -18,7 +17,6 @@ function postStateSwitch() window.title = "LETHAL LOGS";
 function destroy() {
     FlxG.width = FlxG.initialWidth = 1280;
 	window.resize(FlxG.width, FlxG.height);
-	window.resizable = false;
 	for (camera in FlxG.cameras.list) camera.setSize(FlxG.width, FlxG.height);
 
 	window.x = (Capabilities.screenResolutionX / 2) - (window.width / 2);
